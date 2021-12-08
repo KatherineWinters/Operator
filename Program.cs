f91be16947ef6d16f491414f16bda304b2b578a8
Katherine Winters <goble.katherine@gmail.com>
2021-12-07 18:53:48 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using OperatorExercise;
+using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.Write("Enter the radius: ");
+            string input = Console.ReadLine();
+            double radius = double.Parse(input);
+            double area = AreaOfCircle(radius);
+            Console.WriteLine(area);
+        }
+
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+    }
+}
+
